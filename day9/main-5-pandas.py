faea5f239960ff89c872c1236b2eda02da91d55f
Prinu Dickson <prinudickson@gmail.com>
2022-12-10 15:32:27 +0100

    using generator function

--- day9/main-5-pandas.py
+++ day9/main-5-pandas.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import math
 
-input_df = open('input-dev.txt', 'r')
+input_df = open('input.txt', 'r')
 lines = input_df.readlines()
 input_df.close()
 
@@ -82,80 +82,147 @@ print(route_list)
 route_list_flat = [item for sublist in route_list for item in sublist]
 
 
-tracker_path = [[0, 0]]
 start = [0, 0]
 
-for l in route_list_flat:
-    if l == tracker_path[-1]:
-        pass
-    else:
-        # print("************")
-        new_trace = ''
-        #print(l, tracker_path[-1])
 
-        v = l[1] - tracker_path[-1][1]
-        # print(v)
-        h = l[0] - tracker_path[-1][0]
-        #print(h, v)
-        # print(h)
-        if h in [-1, 0, 1] and v in [-1, 0, 1]:
-            # print('within threshold#')
-            #print(h, v)
+def tracker_path_calc(route_list_flat):
+    prev_value = [0, 0]
+    for l in route_list_flat:
+        if l == prev_value:
             pass
-        elif h == 2 and v in [0, 1]:
-            # print("horizantal movement here#")
-            #print(h, v)
-            new_trace = [tracker_path[-1][0]+h-1, tracker_path[-1][1]+v]
-            tracker_path.append(new_trace)
-            # print(tracker_path)
-        elif h in [0, 1] and v == 2:
-            # print("Vertical movement here#")
-            #print(h, v)
-            new_trace = [tracker_path[-1][0]+h, tracker_path[-1][1]+v-1]
-            tracker_path.append(new_trace)
-            # print(tracker_path)
-        # elif h in [0, -1] and v in [0, -1]:
-        #     print('within threshold#')
-        #     print(h, v)
-        #     pass
-        elif h == -2 and v in [-1, 0]:
-            #print("Negative horizantal movement here$$$$$$$$$$$")
-            #print(h, v)
-            new_trace = [tracker_path[-1][0]+h+1, tracker_path[-1][1]+v]
-            tracker_path.append(new_trace)
-            # print(tracker_path)
-        elif h == -2 and v in [1, 0]:
-            # print("Negative horizantal POSITIVE VERTICAL movement here#")
-            #print(h, v)
-            new_trace = [tracker_path[-1][0]+h+1, tracker_path[-1][1]+v]
-            tracker_path.append(new_trace)
-            # print(tracker_path)
-        elif h == 2 and v in [-1, 0]:
-            #print("POsitive Horizontal, Negative Vertical movement here")
-            #print(h, v)
-            new_trace = [tracker_path[-1][0]+h-1, tracker_path[-1][1]+v]
-            tracker_path.append(new_trace)
-            # print(tracker_path)
-        elif h in [-1, 0] and v == -2:
-            #print("Negative Vertical movement here")
-            #print(h, v)
-            new_trace = [tracker_path[-1][0]+h, tracker_path[-1][1]+v+1]
-            tracker_path.append(new_trace)
-            # print(tracker_path)
-        elif h in [1, 0] and v == -2:
-            # print("Negative Vertical POSITIVE horizontal movement here#")
-            #print(h, v)
-            new_trace = [tracker_path[-1][0]+h, tracker_path[-1][1]+v+1]
-            tracker_path.append(new_trace)
-            # print(tracker_path)
         else:
-            pass
+            new_trace = ''
+            v = l[1] - prev_value[1]
+            h = l[0] - prev_value[0]
+            if h in [-1, 0, 1] and v in [-1, 0, 1]:
+                pass
+            elif h == 2 and v in [0, 1]:
+                new_trace = [prev_value[0]+h-1, prev_value[1]+v]
+                print(new_trace)
+                prev_value = new_trace.copy()
+                # tracker_path.append(new_trace)
+                yield new_trace
+            elif h in [0, 1] and v == 2:
+                new_trace = [prev_value[0]+h, prev_value[1]+v-1]
+                print(new_trace)
+                prev_value = new_trace.copy()
+                # tracker_path.append(new_trace)
+                yield new_trace
+            elif h == -2 and v in [-1, 0]:
+                new_trace = [prev_value[0]+h+1, prev_value[1]+v]
+                print(new_trace)
+                prev_value = new_trace.copy()
+                # tracker_path.append(new_trace)
+                yield new_trace
+            elif h == -2 and v in [1, 0]:
+                new_trace = [prev_value[0]+h+1, prev_value[1]+v]
+                print(new_trace)
+                prev_value = new_trace.copy()
+                # tracker_path.append(new_trace)
+                yield new_trace
+            elif h == 2 and v in [-1, 0]:
+                new_trace = [prev_value[0]+h-1, prev_value[1]+v]
+                print(new_trace)
+                prev_value = new_trace.copy()
+                # tracker_path.append(new_trace)
+                yield new_trace
+            elif h in [-1, 0] and v == -2:
+                new_trace = [prev_value[0]+h, prev_value[1]+v+1]
+                print(new_trace)
+                prev_value = new_trace.copy()
+                # tracker_path.append(new_trace)
+                yield new_trace
+            elif h in [1, 0] and v == -2:
+                new_trace = [prev_value[0]+h, prev_value[1]+v+1]
+                print(new_trace)
+                prev_value = new_trace.copy()
+                # tracker_path.append(new_trace)
+                yield new_trace
+            else:
+                pass
 
-# print(tracker_path)
+
+trace_generator = tracker_path_calc(route_list_flat)
+
+# for l in route_list_flat:
+#     if l == tracker_path[-1]:
+#         pass
+#     else:
+#         # print("************")
+#         new_trace = ''
+#         #print(l, tracker_path[-1])
+
+#         v = l[1] - tracker_path[-1][1]
+#         # print(v)
+#         h = l[0] - tracker_path[-1][0]
+#         #print(h, v)
+#         # print(h)
+#         if h in [-1, 0, 1] and v in [-1, 0, 1]:
+#             # print('within threshold#')
+#             #print(h, v)
+#             pass
+#         elif h == 2 and v in [0, 1]:
+#             # print("horizantal movement here#")
+#             #print(h, v)
+#             new_trace = [tracker_path[-1][0]+h-1, tracker_path[-1][1]+v]
+#             tracker_path.append(new_trace)
+#             # print(tracker_path)
+#         elif h in [0, 1] and v == 2:
+#             # print("Vertical movement here#")
+#             #print(h, v)
+#             new_trace = [tracker_path[-1][0]+h, tracker_path[-1][1]+v-1]
+#             tracker_path.append(new_trace)
+#             # print(tracker_path)
+#         # elif h in [0, -1] and v in [0, -1]:
+#         #     print('within threshold#')
+#         #     print(h, v)
+#         #     pass
+#         elif h == -2 and v in [-1, 0]:
+#             #print("Negative horizantal movement here$$$$$$$$$$$")
+#             #print(h, v)
+#             new_trace = [tracker_path[-1][0]+h+1, tracker_path[-1][1]+v]
+#             tracker_path.append(new_trace)
+#             # print(tracker_path)
+#         elif h == -2 and v in [1, 0]:
+#             # print("Negative horizantal POSITIVE VERTICAL movement here#")
+#             #print(h, v)
+#             new_trace = [tracker_path[-1][0]+h+1, tracker_path[-1][1]+v]
+#             tracker_path.append(new_trace)
+#             # print(tracker_path)
+#         elif h == 2 and v in [-1, 0]:
+#             #print("POsitive Horizontal, Negative Vertical movement here")
+#             #print(h, v)
+#             new_trace = [tracker_path[-1][0]+h-1, tracker_path[-1][1]+v]
+#             tracker_path.append(new_trace)
+#             # print(tracker_path)
+#         elif h in [-1, 0] and v == -2:
+#             #print("Negative Vertical movement here")
+#             #print(h, v)
+#             new_trace = [tracker_path[-1][0]+h, tracker_path[-1][1]+v+1]
+#             tracker_path.append(new_trace)
+#             # print(tracker_path)
+#         elif h in [1, 0] and v == -2:
+#             # print("Negative Vertical POSITIVE horizontal movement here#")
+#             #print(h, v)
+#             new_trace = [tracker_path[-1][0]+h, tracker_path[-1][1]+v+1]
+#             tracker_path.append(new_trace)
+#             # print(tracker_path)
+#         else:
+#             pass
+print("route list")
+print(route_list_flat)
+print("traces")
+print(trace_generator)
 print("$$$$Length of the initial route path")
 print(len(route_list))
 print("$$$$Length of th etracker path")
-print(len(tracker_path))
+# print(len(trace_generator))
+i = 0
+for x in trace_generator:
+    # print(x)
+    i = i+1
+
+print(i)
 
 # for index, rl in enumerate(route_list):
 #     if rl == []:
@@ -167,19 +234,19 @@ print(len(tracker_path))
 # print(route_list_flat)
 
 # print(data_df)
-uniques = []
+# uniques = []
 
-#prev_val = ''
-for i in tracker_path:
-    # print(i)
-    # print(uniques)
-    if i in uniques:
-        #print("appending this")
-        # print(uniques)
-        pass
-    else:
-        uniques.append(i)
+# #prev_val = ''
+# for i in tracker_path:
+#     # print(i)
+#     # print(uniques)
+#     if i in uniques:
+#         #print("appending this")
+#         # print(uniques)
+#         pass
+#     else:
+#         uniques.append(i)
 
-# print(uniques)
-print("length of uniques")
-print(len(uniques))
+# # print(uniques)
+# print("length of uniques")
+# print(len(uniques))
